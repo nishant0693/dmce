87aa71c29c2d315e6de706281ce0b34a09bbc6a3
nishant0693 <nishant.0693@gmail.com>
2024-01-21 15:54:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! from DMCE and Nishant. all participant'
+    return 'Hello, World! from DMCE and Nishant. all participant and attentede'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
